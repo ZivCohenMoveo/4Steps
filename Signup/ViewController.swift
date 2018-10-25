e8ceed4d6859b6c0cac4f860353292edc88f0f60
Ziv Cohen <zivc@moveo.co.il>
2018-10-25 10:24:49 +0300

    Initial Commit

---
+++ Signup/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Signup
+//
+//  Created by matan elimelech on 25/10/2018.
+//  Copyright © 2018 Moveo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
